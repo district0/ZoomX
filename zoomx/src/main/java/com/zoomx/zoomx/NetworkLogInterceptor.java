d1e6ddc0109ffb869ef1321208ca1c450a72d78b
Ahmed Fathallah <ahmed.fathallah-ahmed@vodafone.com>
2017-11-19 17:48:06 +0200

    Add Retrofit sdk

---
+++ zoomx/src/main/java/com/zoomx/zoomx/NetworkLogInterceptor.java
@@ -0,0 +1,20 @@
+package com.zoomx.zoomx;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Response;
+
+/**
+ * Created by Ahmed Fathallah on 11/19/2017.
+ */
+
+public class NetworkLogInterceptor implements Interceptor {
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+
+        return null;
+    }
+
+}
