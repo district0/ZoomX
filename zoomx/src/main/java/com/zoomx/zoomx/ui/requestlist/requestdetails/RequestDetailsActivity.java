2676f104bfa56db6cdad8d6a7afd79ea6bbc404f
Ibrahim AbdelGawad <iabdelgawaad@gmail.com>
2017-12-13 15:14:52 +0200

    Added details screen

--- zoomx/src/main/java/com/zoomx/zoomx/ui/requestlist/requestdetails/RequestDetailsActivity.java
+++ zoomx/src/main/java/com/zoomx/zoomx/ui/requestlist/requestdetails/RequestDetailsActivity.java
@@ -1,10 +1,13 @@
 package com.zoomx.zoomx.ui.requestlist.requestdetails;
 
-/**
- * Created by Ibrahim AbdelGawad on 12/3/2017.
- */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class RequestDetailsActivity
-{
+public class RequestDetailsActivity extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_details);
+    }
 }
---
+++ zoomx/src/main/java/com/zoomx/zoomx/ui/requestlist/requestdetails/RequestDetailsAdapter.java
@@ -0,0 +1,8 @@
+package com.zoomx.zoomx.ui.requestlist.requestdetails;
+
+/**
+ * Created by Ibrahim AbdelGawad on 12/13/2017.
+ */
+
+public class RequestDetailsAdapter {
+}
