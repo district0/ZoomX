39aaa93a7c2fb73883bc61694cfa2ca6f897c797
Ahmed Fathallah <fathallah92@users.noreply.github.com>
2017-12-17 15:33:29 +0200

    Add Main menu in Zoomx (#6)
    * Add main menu to the library
    * Fix issues in pull request #6

--- app/src/main/java/com/zoomx/zoomx/MainActivity.java
+++ app/src/main/java/com/zoomx/zoomx/MainActivity.java
@@ -1,14 +1,7 @@
 package com.zoomx.zoomx;
 
-import android.content.Intent;
-import android.graphics.PixelFormat;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Gravity;
-import android.view.MotionEvent;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -16,7 +9,6 @@ import android.widget.Toast;
 import com.zoomx.zoomx.model.User;
 import com.zoomx.zoomx.retrofit.ApiService;
 import com.zoomx.zoomx.retrofit.NetworkManager;
-import com.zoomx.zoomx.ui.requestlist.request.RequestActivity;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -39,7 +31,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startOverlay();
         startUsersService();
     }
 
@@ -78,79 +69,7 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    public void startOverlay() {
-        overLayImage = new ImageView(this);
-        overLayImage.setImageResource(R.drawable.ic_launcher_background);
-        final WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                80,
-                80,
-                WindowManager.LayoutParams.TYPE_PHONE,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
-                PixelFormat.TRANSLUCENT);
 
-        //Specify the view position
-        params.gravity = Gravity.CENTER | Gravity.RIGHT;        //Initially view will be added to top-left corner
-        params.x = 0;
-        params.y = 100;
-
-        wm = (WindowManager) getSystemService(WINDOW_SERVICE);
-
-        overLayImage.setOnTouchListener(new View.OnTouchListener() {
-
-            private int initialX;
-            private int initialY;
-            private float initialTouchX;
-            private float initialTouchY;
-
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-
-
-                        //remember the initial position.
-                        initialX = params.x;
-                        initialY = params.y;
-
-
-                        //get the touch location
-                        initialTouchX = event.getRawX();
-                        initialTouchY = event.getRawY();
-
-                        return false;
-
-                    case MotionEvent.ACTION_MOVE:
-                        //Calculate the X and Y coordinates of the view.
-                        params.x = initialX + (int) (event.getRawX() - initialTouchX);
-                        params.y = initialY + (int) (event.getRawY() - initialTouchY);
-
-
-                        //Update the layout with new X & Y coordinate
-                        wm.updateViewLayout(overLayImage, params);
-                        return false;
-                }
-                return false;
-            }
-        });
-        overLayImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RequestActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // Check if Android M or higher
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            // Show alert dialog to the user saying a separate permission is needed
-            // Launch the settings activity if the user prefers
-            Intent myIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
-            startActivity(myIntent);
-            wm.addView(overLayImage, params);
-        } else {
-            wm.addView(overLayImage, params);
-        }
-    }
 
     @Override
     protected void onDestroy() {
--- zoomx/src/main/java/com/zoomx/zoomx/manager/ZoomX.java
+++ zoomx/src/main/java/com/zoomx/zoomx/manager/ZoomX.java
@@ -1,30 +1,31 @@
 package com.zoomx.zoomx.manager;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 
 import com.zoomx.zoomx.db.AppDatabase;
 import com.zoomx.zoomx.db.RequestDao;
+import com.zoomx.zoomx.ui.menu.ZoomxMenuService;
 
 /**
  * Created by Ahmed Fathallah on 12/10/2017.
  */
 
-public class ZoomX {
+public final class ZoomX {
 
     private static Config config;
-    private static AppDatabase database;
     private static RequestDao requestDao;
 
     public static void init(Config config) {
         ZoomX.config = config;
         setupDataBase();
+        config.getContext().startService(new Intent(config.getContext(), ZoomxMenuService.class));
     }
 
     private static void setupDataBase() {
-        database = AppDatabase.get(config.getContext());
+        AppDatabase database = AppDatabase.get(config.getContext());
         requestDao = database.requestDao();
     }
-
 
     @NonNull
     public static RequestDao getRequestDao() {
