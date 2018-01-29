fefa1bba8c317c6d8102d92ae71ab827243adcb5
Ahmed Fathallah <ahmedfathalla02@gmail.com>
2018-01-29 15:44:30 +0200

    Add showMenuOnStart switch to the setting screen

---
+++ zoomx/src/main/java/com/zoomx/zoomx/util/constants/PrefConstants.java
@@ -0,0 +1,13 @@
+package com.zoomx.zoomx.util.constants;
+
+/**
+ * Created by Ahmed Fathallah on 1/29/2018.
+ */
+
+public class PrefConstants {
+
+    public static final String NETWORK_TRACKER_KEY = "networkTrackerKey";
+    public static final String SHOW_MENU_ON_APP_START_KEY = "showMenuOnAppStart";
+
+
+}
