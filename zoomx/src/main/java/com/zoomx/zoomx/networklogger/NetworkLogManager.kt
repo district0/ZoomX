63aabefbdb860aa4b87fa98cfd955d7b142059b2
Mohamed Ibrahim <mibrahim.dev@gmail.com>
2018-12-08 22:02:06 +0200

    convert class to Kotlin

---
+++ zoomx/src/main/java/com/zoomx/zoomx/networklogger/NetworkLogManager.kt
@@ -0,0 +1,28 @@
+package com.zoomx.zoomx.networklogger
+
+import com.zoomx.zoomx.config.ZoomX
+import com.zoomx.zoomx.model.RequestEntity
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
+
+/**
+ * Created by Ahmed Fathallah on 1/11/2018.
+ */
+
+object NetworkLogManager {
+
+    fun log(builder: RequestEntity.Builder?) {
+        if (builder != null)
+            insertRequest(builder).subscribe()
+    }
+
+    private fun insertRequest(builder: RequestEntity.Builder): Completable {
+        return Completable
+                .fromAction { ZoomX.getRequestDao().insertRequest(builder.create()) }
+                .doOnError(Consumer<Throwable> { Timber.e(it) })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+    }
+}
