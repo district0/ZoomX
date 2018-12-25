2cc2ed1bce4cebf61e30572e3ac8afd0ae9a2061
Ibrahim AbdelGawad <ibrahim.abdelgawad-mohamed@vodafone.com>
2018-12-25 15:42:49 +0300

    Fixed request body for special project

--- zoomx/src/main/java/com/zoomx/zoomx/util/FormatUtil.java
+++ zoomx/src/main/java/com/zoomx/zoomx/util/FormatUtil.java
@@ -1,9 +1,13 @@
 package com.zoomx.zoomx.util;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
+
+import okhttp3.RequestBody;
+import okio.Buffer;
 
 /**
  * Created by Ibrahim AbdelGawad on 12/12/2017.
@@ -24,6 +28,20 @@ public class FormatUtil {
             }
         }
         return formattedString;
+    }
+
+    public static String bodyToString(final RequestBody request){
+        try {
+            final Buffer buffer = new Buffer();
+            if(request != null)
+                request.writeTo(buffer);
+            else
+                return "";
+            return buffer.readUtf8();
+        }
+        catch (final IOException e) {
+            return "did not work";
+        }
     }
 
 }
